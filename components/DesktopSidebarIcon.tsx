48779032447c18a3e6a7dc104678d70cefc735bc
Jairo <jartmatm@gmail.com>
2026-05-08 06:57:28 +1000

    Align utility sidebar icons with home

--- components/DesktopSidebarIcon.tsx
+++ components/DesktopSidebarIcon.tsx
@@ -60,9 +60,14 @@ export function DesktopSidebarIcon({ type, className = 'h-5 w-5' }: DesktopSideb
 
   if (type === 'topup') {
     return (
-      <svg viewBox="0 0 24 24" {...common}>
-        <path d="M12 5v14" />
-        <path d="M5 12h14" />
+      <svg viewBox="0 0 24 24" className={className} fill="none">
+        <path
+          d="M6 9.8835H4.5C3.67157 9.8835 3 9.20482 3 8.36762V8.16524C3 7.36649 3.46547 6.64266 4.18772 6.31826L11.1877 3.1742C11.7049 2.94193 12.2951 2.94193 12.8123 3.1742L19.8123 6.31826C20.5345 6.64266 21 7.36649 21 8.16524V8.36762C21 9.20482 20.3284 9.8835 19.5 9.8835H18M6 9.8835V16.9576M6 9.8835H10M6 16.9576H4.5C3.67157 16.9576 3 17.6363 3 18.4735V19.4841C3 20.3213 3.67157 21 4.5 21H19.5C20.3284 21 21 20.3213 21 19.4841V18.4735C21 17.6363 20.3284 16.9576 19.5 16.9576H18M6 16.9576H10M18 9.8835V16.9576M18 9.8835H14M18 16.9576H14M14 9.8835V16.9576M14 9.8835H10M14 16.9576H10M10 9.8835V16.9576"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     );
   }
@@ -70,9 +75,7 @@ export function DesktopSidebarIcon({ type, className = 'h-5 w-5' }: DesktopSideb
   if (type === 'withdraw') {
     return (
       <svg viewBox="0 0 24 24" {...common}>
-        <path d="M12 5v14" />
-        <path d="m6 13 6 6 6-6" />
-        <path d="M5 5h14" />
+        <path d="M12 6V14M12 14L14.5 11.5M12 14L9.5 11.5M15.5 8H17C18.6569 8 20 9.34315 20 11V15C20 16.6569 18.6569 18 17 18H7C5.34315 18 4 16.6569 4 15V11C4 9.34315 5.34315 8 7 8H8.5" />
       </svg>
     );
   }
